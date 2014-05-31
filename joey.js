1e3ead3819cd043711d61114c6bf649e336c89d2
ljv5555 <jjverderber@gmail.com>
2014-05-31 10:56:32 -0500

    Create joey.js

---
+++ joey.js
@@ -0,0 +1,7 @@
+/* joey.js - (C) 2014 https://github.com/ljv5555/joey.js */
+/* This software may be used under the terms of the LGPL2.1 - see http://ljv5555.github.io/joey.js/LICENSE */
+window.joey=(function(){
+  var rtn = {};
+  
+  retyrn rtn;
+})();
