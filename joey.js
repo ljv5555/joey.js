8d2f7c0e9b0d1c783cd55d62bcfd22d7fad01f8d
ljv5555 <jjverderber@gmail.com>
2014-05-31 11:44:04 -0500

    Update joey.js

--- joey.js
+++ joey.js
@@ -22,5 +22,27 @@ window.joey=(function(){
   };
   rtn.when=when;
   
+  /**
+   * adds script tag as the last element in the parent node of the last script tag
+   **/ 
+  var getXsS = function(url,donotpreventcache)
+  {
+    if(donotpreventcache){donotpreventcache=true;}else{donotpreventcache=false;}
+    var ss = 's' + 'cr' + 'ip' + 't';
+    var cst = document.getElementsByTagName(ss)[document.getElementsByTagName(ss).length-1];
+    var e = document.createElement(ss);
+    e.async='async';
+    var tsstr = '';
+    if(donotpreventcache===false)
+    {
+      var ts = (new Date()).getTime();
+      tsstr = '_ts1_='+ts;
+      if((''+url).indexOf('?')==-1){tsstr='?'+tsstr;}else{tsstr='&'+tsstr;}
+    }
+    var url2 = url+tsstr;
+    e.src=url2;
+    cst.parentNode.parentElement.appendChild(e);
+  };
+
   return rtn;
 })();
