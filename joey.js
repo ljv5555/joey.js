29501e2341fd33c4e14b794f8079cf378510bd76
ljv5555 <jjverderber@gmail.com>
2014-05-31 11:17:30 -0500

    Update joey.js

--- joey.js
+++ joey.js
@@ -2,6 +2,25 @@
 /* This software may be used under the terms of the LGPL2.1 - see http://ljv5555.github.io/joey.js/LICENSE */
 window.joey=(function(){
   var rtn = {};
+  var when=function(condition,callback,cbparams)
+  {
+      if("function"!=typeof(condition))
+      {
+        var conditionstr = ''+condition;
+        condition=function(){return eval(conditionstr);}
+      }
+      if("function"!=typeof(callback))
+      {
+        var callbackstr = ''+callback;
+        callback=function(){return eval(callbackstr);}
+      }
+      if(condition()){callback(cbparams);}
+      else
+      {
+        setTimeout(function(){when(condition,callback,cbparams);},100);
+      }
+  };
+  rtn.when=when;
   
-  retyrn rtn;
+  return rtn;
 })();
