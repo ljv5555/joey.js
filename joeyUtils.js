e95fdefb2111e383c7ed0e02d025647cdb866542
ljv5555 <jjverderber@gmail.com>
2014-06-06 08:45:28 -0500

    Create joeyUtils.js

---
+++ joeyUtils.js
@@ -0,0 +1,150 @@
+var joeyUtils = (new (function(){
+	/**
+	 * 
+	 * @returns
+	 */
+    var getUrlParams = function() {
+        var query = window.location.search;
+        var re = /([^&=]+)=?([^&]*)/g;
+        var decode = function(str){return decodeURIComponent(str.replace(/\+/g, ' '));};
+        var params = {}, e;
+        if (query) {
+            if (query.substr(0, 1) == '?') {
+                query = query.substr(1);
+            }
+
+            while (e = re.exec(query)) {
+                var k = decode(e[1]);
+                var v = decode(e[2]);
+                if (params[k] !== undefined) {
+                    if (!jQuery.isArray(params[k])) {
+                        params[k] = [params[k]];
+                    }
+                    params[k].push(v);
+                } else {
+                    params[k] = v;
+                }
+            }
+        }
+        return params;
+    };
+
+
+    /**
+     * 
+     * @param o
+     * @returns
+     */
+	var log = function(o) {
+		try {
+			console.log(o);
+		} catch (e) {
+		}
+	};
+
+
+
+
+
+	/**
+	 * 
+	 * @param url
+	 * @param callback
+	 * @param errorCallback
+	 * @param requestParameters
+	 * @param doNotSend
+	 * @returns
+	 */
+	var getRequest = function(url, callback, errorCallback, requestParameters, doNotSend) {
+		var xhr = new XMLHttpRequest();
+		if (requestParameters) {
+			if (url.indexOf('?') == -1) {
+				url = url + '?';
+			} else {
+				url = url + '&';
+			}
+			url = url + jQuery.param(requestParameters);
+		}
+		xhr.open('GET', url);
+		xhr.setRequestHeader('Authorization', 'Bearer ' + atoken);
+		xhr.onload = function() {
+			callback(xhr.responseText);
+		};
+		if (errorCallback) {
+			xhr.onerror = function(p) {
+				errorCallback(p);
+			};
+		} else {
+			xhr.onerror = function(p) {
+				alert('Request error: \n' + url + '\nSee console log.');
+				try {
+					console.log(p);
+				} catch (ex) {
+				}
+			};
+		}
+		if( !doNotSend ){xhr.send();}
+		return xhr;
+	};
+
+	/**
+	 * 
+	 * @param url
+	 * @param callback
+	 * @param errorCallback
+	 * @param requestParameters
+	 * @param doNotSend
+	 * @returns
+	 */
+	var getJSON = function(url, callback, errorCallback, requestParameters, doNotSend) 
+	{
+		return getRequest(url, function(e){callback(JSON.parse(e))}, errorCallback, requestParameters, doNotSend);
+	};
+
+	/**
+	 * Synchronous get request
+	 * @param urlToGet
+	 * @param parametersToUse
+	 * @returns
+	 */
+	var sGetRequest = function( urlToGet, parametersToUse )
+	{
+		if( !parametersToUse ){ parametersToUse = {}; }
+		var rtn = '';
+		try{
+		log('running sGetRequest("'+urlToGet+'",'+JSON.stringify(parametersToUse)+");");
+		rtn = jQuery.ajax({
+		   url:urlToGet,
+		   data:parametersToUse,
+		   async:false,
+		   headers:{'Authorization': ('Bearer ' + gapi.auth.getToken().access_token)}
+		   }).responseText;
+		   log('returning: ');
+		   log(''+rtn);
+		}catch(e)
+		{
+			//if(error){error('Error: '+e.message);}
+			log('Error: ');
+			log(''+e.message);
+		}
+		return rtn;
+	};
+	var sGetJSON = function(urlToGet, parametersToUse)
+	{ 
+		return JSON.parse(sGetRequest( urlToGet, parametersToUse ) );
+	};
+	
+	var exportLocal = function(targetObject,localObjectNvPairArray1)
+	{
+		var t = targetObject;
+		for(var i = 1;i<arguments.length;i++)
+		{
+			var n = arguments[i][0];
+			var v = arguments[i][1];
+			targetObject[n]=v;
+		}
+	};
+	exportLocal(this,['getRequest',getRequest],['getJSON',getJSON],['sGetRequest',sGetRequest],['sGetJSON',sGetJSON],['getUrlParams',getUrlParams]);
+	
+	
+})());
